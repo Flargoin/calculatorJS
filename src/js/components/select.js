b87fb897648fb6f3b164eb4305e1f3bb3ff6ee7e
Sergey <flargoin@gmail.com>
2023-10-14 21:30:21 +0300

    Добавил логику для селектов

--- src/js/components/select.js
+++ src/js/components/select.js
@@ -3,6 +3,7 @@ const select = (selectHeader, selectItem, selectSelector, activeClass) => {
         selectItems = document.querySelectorAll(selectItem),
         elSelect = document.querySelectorAll(selectSelector);
 
+  /* Функции */
   // Удаление активного класса
   const selectClose = function () {
     elSelect.forEach(item => {
@@ -12,6 +13,8 @@ const select = (selectHeader, selectItem, selectSelector, activeClass) => {
 
   // Открытие-закрытие селекта
   const selectToggle = function () {
+    // При открытии второго и более селектов, предыдущие будут закрываться.
+    selectClose();
     this.parentElement.classList.toggle(activeClass);
   };
 
@@ -23,6 +26,8 @@ const select = (selectHeader, selectItem, selectSelector, activeClass) => {
         selectClose();
   };
 
+
+  /* Обработчики */
   // Обработчик события клика на селект, для открытия и закрытия
   selectTriggers.forEach(selectTrigger => {
     selectTrigger.addEventListener("click", selectToggle);
@@ -33,7 +38,13 @@ const select = (selectHeader, selectItem, selectSelector, activeClass) => {
     item.addEventListener("click", selectChoose);
   });
 
-
+  // Обработчик клика вне селекта
+  window.addEventListener('click', event => {
+    const target = event.target;
+    if (!target.closest(selectSelector) && !target.closest(selectHeader)) { // если этот элемент или его родительские элементы не окно навигации и не кнопка
+      selectClose();
+    }
+  });
 }
 
 export default select;
