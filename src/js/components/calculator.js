f7fb3db1d4fd8ce9d5d59b1db81a7d39cb4a3165
Romanov Sergey <flargoin@gmail.com>
2023-10-22 22:03:11 +0300

    Новый селект, нужно доработать

--- src/js/components/calculator.js
+++ src/js/components/calculator.js
@@ -1,11 +1,75 @@
-import 'nouislider';
-import rangeSlider from './range-slider';
+import noUiSlider from 'nouislider';
 
 const calculator = () => {
-/*     rangeSlider.noUiSlider.on("change", () => {
-      calcQuantity.textContent = rangeSlider.noUiSlider.get();
-    }); */
+
+  const rangeSlider = document.querySelector('#range-slider');
+
+  let calcQuantity = document.querySelector("#calculator__window-count");
+
+  // Создание range slider
+  noUiSlider.create(rangeSlider, {
+    start: 3,
+    connect: "upper",
+    tooltips: true,
+    step: 1,
+    format: {
+      to: function ( value ) {
+        return value.toFixed();;
+      },
+      from: function ( value ) {
+        return value;
+      }
+    },
+    range: {
+        'min': 1,
+        'max': 10
+    }
+  });
+
+  // Цены от 1 до 10 штук Панельно/блочный дом
+  const block = [2035, 3245, 4345, 5665, 6875, 8085, 9295, 10505, 11715, 12925];
+  // Цены от 1 до 10 штук Кирпичный с одинарной рамой
+  const brickOne = [ 2310, 3520, 4730, 5940, 7150, 8360, 9570, 10780, 13145, 14190];
+  // Цены от 1 до 10 штук Кирпичный с двойной рамой
+  const brickTwo = [ 3245, 5808, 8580, 10945, 13530, 16115, 17545, 20185, 22275, 23595 ];
+
+  const place = {
+      fiveKm : 400,
+      tenKm : 900,
+      fifKm : 1500,
+  };
+
+  // взаимодействие с интерфесомкол-во окон
+  rangeSlider.noUiSlider.on("update", () => {
+    calcQuantity.textContent = rangeSlider.noUiSlider.get();
+  });
+
+/*   function calculate() {
+    const placeCurrent = document.querySelector("#calc-place-select .select__current"),
+          typeCurrent = document.querySelector("#calc-type-select .select__current");
+          placeEl = document.querySelector('#calculator__place'),
+          typeEl = document.querySelector('#calculator__house-type');
+
+    let placePrice = 0,
+        sum = 0,
+        countPrice =
+
+    if(placeCurrent.textContent == "до 5 км от МКАД") {
+      placePrice = 400;
+    } else if(placeCurrent.textContent == "до 10 км от МКАД") {
+      placePrice = 900;
+    } else if(placeCurrent.textContent == "до 15 км от МКАД") {
+      placePrice = 1500;
+    } else {
+      placePrice = 0;
+    }
+
+    if(typeCurrent.textContent == "Кирпичный с одинарной рамой") {
+
+    }
+  } */
 
 }
 
 export default calculator;
+
--- src/js/components/select.js
+++ src/js/components/select.js
@@ -1,65 +1,29 @@
-const select = (selectHeader, selectItem, selectSelector, activeClass) => {
-  const selectTriggers = document.querySelectorAll(selectHeader),
-        selectItems = document.querySelectorAll(selectItem),
-        elSelect = document.querySelectorAll(selectSelector);
+const select = (selector) => {
+  const selectEl = document.querySelectorAll(selector),
+        selectOptionPlace = document.querySelectorAll("#select-dropdown-place li"),
+        selectOptionType = document.querySelectorAll("#select-dropdown-type li"),
+        selectCurrentValue = document.querySelectorAll(".select-button");
 
-  // Элементы блока расчёта
-  const calcQuantity = document.querySelector("#calculator__window-count"),
-        calcPlace = document.querySelector("#calculator__place"),
-        calcHouseType = document.querySelector("#calculator__house-type"),
-        calcTotalPrice = document.querySelector("#calculator__total-price");
+  console.log(selectCurrentValue);
 
-  /* Функции */
-  // Удаление активного класса
-  const selectClose = function () {
-    elSelect.forEach(item => {
-      item.classList.remove(activeClass);
+  selectEl.forEach(item => {
+    item.addEventListener("click", () => {
+      item.classList.toggle("is-active");
     });
-  }
-
-  // Открытие-закрытие селекта
-  const selectToggle = function () {
-    selectClose();
-
-    if(!this.classList.contains(activeClass)) {
-      this.parentElement.classList.add(activeClass);
-    }
-  };
-
-  // Выбор option из селекта
-  const selectChoose = function () {
-    let text = this.textContent,
-        currentText = this.closest(selectSelector).querySelector(".select__current");
-        currentText.textContent = text;
-
-    if(this.closest("#calc-place-select")) {
-      calcPlace.textContent = text;
-    } else {
-      calcHouseType.textContent = text;
-    }
-
-    selectClose();
-  };
-
-
-  /* Обработчики */
-  // Обработчик события клика на селект, для открытия и закрытия
-  selectTriggers.forEach(trigger => {
-    trigger.addEventListener("click", selectToggle);
   });
 
-  // Обработчик выбора option
-  selectItems.forEach(item => {
-    item.addEventListener("click", selectChoose);
+  selectOptionPlace.forEach(option => {
+    option.addEventListener("click", (event) => {
+      selectCurrentValue.textContent = event.target.textContent;
+    });
   });
 
-  // Обработчик клика вне селекта
-  window.addEventListener('click', event => {
-    const target = event.target;
-    if (!target.closest(selectSelector) && !target.closest(selectHeader)) {
-      selectClose();
-    }
+  selectOptionType.forEach(option => {
+    option.addEventListener("click", (event) => {
+      selectCurrentValue.textContent = event.target.textContent;
+    });
   });
+
 }
 
 export default select;
